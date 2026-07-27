de553a6f2aa3383f595b8f326a2f53c31ca40c27
ingerutonny-ui <255609125+ingerutonny-ui@users.noreply.github.com>
2026-07-27 19:57:48 -0400

    Update models.py

--- api/models.py
+++ api/models.py
@@ -1,23 +1,23 @@
 from sqlalchemy import Column, Integer, String, Numeric, ForeignKey
-from .database import Base
+from api.database import Base, engine
 
 class Cliente(Base):
     __tablename__ = "cliente_ms"
-    
+
     codigo_cli = Column(String(50), primary_key=True, index=True)
     nombre_cli = Column(String(100))
     num_cel_cli = Column(String(20))
 
 class Producto(Base):
     __tablename__ = "producto_ms"
-    
+
     codigo_pro = Column(String(50), primary_key=True, index=True)
     nombre_pro = Column(String(100))
     precio_pro = Column(Numeric(10, 2))
 
 class Historial(Base):
     __tablename__ = "historial_ms"
-    
+
     codigo_his = Column(Integer, primary_key=True, index=True, autoincrement=True)
     codigo_cli = Column(String(50), ForeignKey("cliente_ms.codigo_cli"))
     lugar_ven = Column(String(100))
@@ -25,9 +25,12 @@ class Historial(Base):
 
 class Venta(Base):
     __tablename__ = "venta_ms"
-    
+
     codigo_ven = Column(Integer, primary_key=True, index=True, autoincrement=True)
     codigo_his = Column(Integer, ForeignKey("historial_ms.codigo_his"))
     codigo_pro = Column(String(50), ForeignKey("producto_ms.codigo_pro"))
     cantidad_ven = Column(Integer)
     total_ven = Column(Numeric(10, 2))
+
+def crear_tablas():
+    Base.metadata.create_all(bind=engine)
