518afcc58bb0e80df3863013340847947a96b3f1
ingerutonny-ui <255609125+ingerutonny-ui@users.noreply.github.com>
2026-07-27 20:14:29 -0400

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from api.models import crear_tablas
-from api.crud import registrar_cliente_db, realizar_compra_db, verificar_cliente_db
+from api.crud import registrar_cliente_db, verificar_cliente_db, realizar_compra_db
 
 app = FastAPI()
 
@@ -13,13 +13,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/api/verificar_cliente/{codigo_cli}")
-def verificar_cliente(codigo_cli: str):
-    try:
-        return verificar_cliente_db(codigo_cli)
-    except Exception as e:
-        raise HTTPException(status_code=404, detail=str(e))
-
 @app.on_event("startup")
 def startup_event():
     crear_tablas()
@@ -29,11 +22,18 @@ def registrar_cliente(data: dict):
     try:
         return registrar_cliente_db(data)
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+        raise HTTPException(status_code=400, detail=str(e))
 
-@app.post("/api/comprar")
+@app.get("/api/verificar_cliente/{codigo_cli}")
+def verificar_cliente(codigo_cli: str):
+    try:
+        return verificar_cliente_db(codigo_cli)
+    except Exception as e:
+        raise HTTPException(status_code=404, detail=str(e))
+
+@app.post("/api/realizar_compra")
 def realizar_compra(data: dict):
     try:
         return realizar_compra_db(data)
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+        raise HTTPException(status_code=400, detail=str(e))
