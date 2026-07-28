cb33a27d1fc52e476d42a397101d43be832f5cb8
ingerutonny-ui <255609125+ingerutonny-ui@users.noreply.github.com>
2026-07-27 20:06:36 -0400

    Update database.py

--- api/database.py
+++ api/database.py
@@ -2,10 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Configura tu URL de conexión a la base de datos de Neon aquí
-SQLALCHEMY_DATABASE_URL = "postgresql://usuario:password@host/database"
+SQLALCHEMY_DATABASE_URL = "postgresql://usuario:tu_contraseña@tu_host_o_servidor/nombre_de_tu_base_de_datos"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+def get_db_connection():
+    db = SessionLocal()
+    return db
